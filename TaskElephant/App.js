4bf23d67212742cf8a71afaf68a4618ddfa0a659
RedRoach51 <RedRoach51@gmail.com>
2021-09-28 11:42:43 -0400

    Added text input

--- TaskElephant/App.js
+++ TaskElephant/App.js
@@ -1,16 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+// [1,2] = useState is a variable declaration. 1 is the 'get' method, 2 is the 'set' method.    
+  const [text, setText] = useState(true);
+  const [number, setNumber] = useState(true);
+  const [anything, setAnything] = useState(true);
+  
+  function _onPressButton(text) {
+    alert(text);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Text> Open up App.js to start working on your app! </Text>
       <Text>Test Application for Task Elephant </Text>
+      <TextInput placeholder="Text entry input here" onChangeText={text => setText(text)}/>
+      <button onClick={() => _onPressButton(text)}>Click here to<br />display input text.</button>
+
 	  <StatusBar style="auto" />
     </View>
+  
   );
 }
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
