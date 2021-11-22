bac60fc358d6569317b52211a76bfbd32ca15500
Keye Li <kli32@stevens.edu>
2021-11-21 21:26:09 -0500

    google login

--- TaskElephant/screens/Welcome.js
+++ TaskElephant/screens/Welcome.js
@@ -3,8 +3,49 @@ import React from 'react';
 import { StyleSheet, View, Text,Button } from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
 
 export default function Welcome({navigation}){
+
+  const [userInfo, setUserInfo] = useState(null);
+  useEffect(()=>{
+    GoogleSignin.configure({
+      scopes: ['https://www.googleapis.com/auth/drive.readonly'], // [Android] what API you want to access on behalf of the user, default is email and profile
+      webClientId: '913936376356-v19lrvg1gu0b8tqou2rdc7eohp1llhcp.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+      offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+      // hostedDomain: '', // specifies a hosted domain restriction
+      forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+      // accountName: '', // [Android] specifies an account name on the device that should be used
+      // iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+      // googleServicePlistPath: '', // [iOS] if you renamed your GoogleService-Info file, new name here, e.g. GoogleService-Info-Staging
+      // openIdRealm: '', // [iOS] The OpenID2 realm of the home web server. This allows Google to include the user's OpenID Identifier in the OpenID Connect ID token.
+      // profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
+    });
+  })
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      setUserInfo({ userInfo });
+      console.log({userInfo});
+      navigation.navigate('EnergyDay');
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
 
   const getAllTasks = async() => {
     /* Returns an array of Task objects stored in AsyncStorage. 
@@ -33,6 +74,10 @@ export default function Welcome({navigation}){
 
   console.log(debug_CurrentTime());
   getAllTasks();
+
+
+
+
   return (
 
 
@@ -48,6 +93,14 @@ export default function Welcome({navigation}){
           <Text style = {styles.name}>
               Task Elephant
           </Text>
+
+          <GoogleSigninButton
+            style={{ width: 192, height: 48 }}
+            size={GoogleSigninButton.Size.Wide}
+            color={GoogleSigninButton.Color.Dark}
+            onPress={signIn}
+            // disabled={this.state.isSigninInProgress}
+          />
 
 
           <Button title = 'Start!' onPress = {pressHandler} style = {styles.button}>
