b8bfa09a40f3dc096f2723ab821503c1a827fe69
g-onelli <63609012+g-onelli@users.noreply.github.com>
2022-04-09 18:19:23 -0400

    Update HomeScreen.js

--- TaskElephant/styling/HomeScreen.js
+++ TaskElephant/styling/HomeScreen.js
@@ -1,24 +1,27 @@
 import { StyleSheet, View, Platform } from "react-native";
-
-
+import originalPalette from './styleConstants/Colors';
+import borderStyle from './styleConstants/Border';
+import textStyling from './styleConstants/Texts';
 
 const welcomeStyle = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: originalPalette.white,
       alignItems: 'center',
       justifyContent: 'center',
-      backgroundColor: 'rgba(115,162,243,.3)',
+      backgroundColor: originalPalette.genSheetBackground,
     },
     topSentence:{
         fontSize:35,
         marginBottom:0,
-        fontFamily: Platform.OS == 'ios'? 'Verdana': 'sans-serif',
+        fontFamily: Platform.OS == 'ios'? textStyling.iosFont: textStyling.androidFont,
+        letterSpacing: 2,
     },
     name : {
         marginBottom:100,
         fontSize:35,
-        fontFamily: Platform.OS == 'ios'? 'Verdana': 'sans-serif',
+        fontFamily: Platform.OS == 'ios'? textStyling.iosFont: textStyling.androidFont,
+        letterSpacing: 2,
     },
 
     button:{
