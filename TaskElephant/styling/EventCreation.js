520e018d54c9e1a4e989408433e00e24e7a1ce06
g-onelli <63609012+g-onelli@users.noreply.github.com>
2022-04-09 11:49:35 -0400

    Update Event Creation

--- TaskElephant/styling/EventCreation.js
+++ TaskElephant/styling/EventCreation.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 
 const eventStyle = StyleSheet.create({
     container: {
@@ -6,6 +6,26 @@ const eventStyle = StyleSheet.create({
         backgroundColor: 'rgba(250,131,50,0.80)',
         alignItems: 'center',
         justifyContent: 'flex-start',
+      },
+
+      childContainer:{
+        flex:1,
+        flexDirection:"row",
+        justifyContent:"space-around",
+        margin:0,
+      },
+
+      childInput:{
+        height: 50,
+        fontSize: 20,
+        borderWidth:1,
+        padding:10,
+        margin:10,
+        marginBottom:0,
+        backgroundColor:'#ededed',
+        borderRadius: 3,
+        elevation:8,
+        width: 160,
       },
     
       textInput:{
@@ -15,15 +35,17 @@ const eventStyle = StyleSheet.create({
         padding:10,
         margin:10,
         width: Platform.OS === 'ios' ? 400 : 375,
-        //fontFamily: Platform.OS == 'ios'? 'Verdana': 'sans-serif',
+        fontFamily: Platform.OS == 'ios'? 'Verdana': 'sans-serif',
         backgroundColor:'#ededed',
+        color: '#000',
         borderRadius: 3,
         elevation:8,
         
       },
     
       buttonView:{
-        marginTop:125,
+        marginTop:200,
+        marginBottom:50,
         fontSize:40
       },
       
@@ -43,6 +65,17 @@ const eventStyle = StyleSheet.create({
         minWidth:'100%',
         margin:20,
         fontSize:20,
+        textAlign:'center',
+        borderWidth:1,
+        color:'#000',
+        fontWeight:'bold'
+      },
+      secondText:{
+        backgroundColor:'rgba(237,237,237,.9)',
+        padding: 10,
+        minWidth:'100%',
+        margin:10,
+        fontSize:15,
         textAlign:'center',
         borderWidth:1,
         fontWeight:'bold'
