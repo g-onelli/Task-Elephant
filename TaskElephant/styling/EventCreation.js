76e74ed1698dbe8fb84aa51265c79ae1f7def63b
g-onelli <63609012+g-onelli@users.noreply.github.com>
2022-04-09 18:18:17 -0400

    Update EventCreation.js

--- TaskElephant/styling/EventCreation.js
+++ TaskElephant/styling/EventCreation.js
@@ -1,9 +1,12 @@
 import { StyleSheet, Platform } from "react-native";
+import originalPalette from './styleConstants/Colors';
+import borderStyle from './styleConstants/Border';
+import textStyling from './styleConstants/Texts';
 
 const eventStyle = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'rgba(250,131,50,0.80)',
+        backgroundColor: originalPalette.eventBackground,
         alignItems: 'center',
         justifyContent: 'flex-start',
       },
@@ -18,29 +21,30 @@ const eventStyle = StyleSheet.create({
       childInput:{
         height: 50,
         fontSize: 20,
-        borderWidth:1,
+        borderWidth:borderStyle.narrowBorder,
         padding:10,
         margin:10,
         marginBottom:0,
-        backgroundColor:'#ededed',
-        borderRadius: 3,
+        backgroundColor: originalPalette.inputColor,
+        borderRadius: borderStyle.smlCurve,
         elevation:8,
         width: 160,
+        letterSpacing: 2,
       },
     
       textInput:{
         height: 50,
         fontSize: 20,
-        borderWidth:1,
+        borderWidth:borderStyle.narrowBorder,
         padding:10,
         margin:10,
         width: Platform.OS === 'ios' ? 400 : 375,
-        fontFamily: Platform.OS == 'ios'? 'Verdana': 'sans-serif',
-        backgroundColor:'#ededed',
-        color: '#000',
-        borderRadius: 3,
+        fontFamily: Platform.OS == 'ios'? textStyling.iosFont: textStyling.androidFont,
+        backgroundColor: originalPalette.inputColor,
+        color: originalPalette.black,
+        borderRadius: borderStyle.smlCurve,
         elevation:8,
-        
+        letterSpacing: 2,
       },
     
       buttonView:{
@@ -60,25 +64,29 @@ const eventStyle = StyleSheet.create({
       },
 
       Text:{
-        backgroundColor:'rgba(237,237,237,.9)',
-        padding: 10,
+        backgroundColor: originalPalette.creationTextBackground,
+        padding: 5,
         minWidth:'100%',
         margin:20,
         fontSize:20,
         textAlign:'center',
-        borderWidth:1,
-        color:'#000',
-        fontWeight:'bold'
+        borderWidth:borderStyle.narrowBorder,
+        color: originalPalette.black,
+        fontWeight:'bold',
+        letterSpacing: 2,
+        fontFamily: Platform.OS == 'ios'? textStyling.iosFont: textStyling.androidFont,
       },
       secondText:{
-        backgroundColor:'rgba(237,237,237,.9)',
+        backgroundColor:originalPalette.creationTextBackground,
         padding: 10,
         minWidth:'100%',
         margin:10,
         fontSize:15,
         textAlign:'center',
-        borderWidth:1,
-        fontWeight:'bold'
+        borderWidth: borderStyle.narrowBorder,
+        fontWeight:'bold',
+        letterSpacing: 2,
+        fontFamily: Platform.OS == 'ios'? textStyling.iosFont: textStyling.androidFont,
       }
     
 });
