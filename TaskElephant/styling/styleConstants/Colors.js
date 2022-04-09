8014d270a84bcb3d501ac82d5f22a4a3c2a4db62
g-onelli <63609012+g-onelli@users.noreply.github.com>
2022-04-09 18:16:31 -0400

    Added color and text constants

---
+++ TaskElephant/styling/styleConstants/Colors.js
@@ -0,0 +1,19 @@
+const originalPalette = {
+    white: '#fff',
+    black:'#000',
+    txtShadow: '#808080',
+    deleteButtonColor: '#FE4643',
+    genSheetBackground: 'rgba(115,162,243,.3)',
+    creationTextBackground: 'rgba(237,237,237,.9)',
+    inputColor: '#ededed',
+    scheduleItemColor: '#ededed',
+    genButtonColor: '#ededed',
+    eventBackground:'rgba(250,131,50,0.80)',
+    eventItemBackground: '#bbb',
+    itemBorderColor: '#bbb',
+    itemTextColor: 'rgba(0,0,0,0.5)',
+    itemBackground: 'rgba(225,225,225,0.3)',
+    taskBackground: '#73a2f3',
+    scheduleTaskItem: 'rgba(115,162,243,.9)',
+}
+export default originalPalette;
---
+++ TaskElephant/styling/styleConstants/Texts.js
@@ -0,0 +1,7 @@
+const textStyling = {
+    iosFont: 'Verdana',
+    androidFont: 'serf-sans',
+    spaceBtwLetters: 2,
+    txtWeight: 'bold',
+    txtTransform: 'capitalize',
+}
