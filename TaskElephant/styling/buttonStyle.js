fad11263e205af0050e977b56351d8e04f9de3ba
g-onelli <63609012+g-onelli@users.noreply.github.com>
2022-04-09 18:17:40 -0400

    Update buttonStyle.js

--- TaskElephant/styling/buttonStyle.js
+++ TaskElephant/styling/buttonStyle.js
@@ -1,5 +1,7 @@
 import { Platform, StyleSheet, View } from "react-native";
-
+import originalPalette from './styleConstants/Colors';
+import borderStyle from './styleConstants/Border';
+import textStyling from './styleConstants/Texts';
 
 const styleButton = StyleSheet.create({
     customButtonContainer:{
@@ -10,23 +12,23 @@ const styleButton = StyleSheet.create({
         justifyContent:'center',
  //       alignItems:'center',
         elevation: 8,
-        backgroundColor: '#fff',
-        borderRadius: 10,
+        backgroundColor: originalPalette.white,
+        borderRadius: borderStyle.lrgCurve,
         padding:10,
         //height:50,
         width: Platform.OS == 'ios' ? 300 : '90%',
         marginBottom:10,
         marginTop:4,
-        borderWidth:2,
+        borderWidth:borderStyle.wideBorder,
         
     },
     customButtonText: {
-        //fontFamily: Platform.OS == 'ios'? 'Verdana': 'sans-serif',
+        fontFamily: Platform.OS == 'ios'? textStyling.iosFont: textStyling.androidFont,
         textTransform:'capitalize',
-        letterSpacing:2,
+        letterSpacing: 2,
         fontSize: 22,
         fontWeight:"bold",
-        color: '#000',
+        color: originalPalette.black,
         textAlign: "center",
     },
 
