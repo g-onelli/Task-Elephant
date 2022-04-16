5884ff98052e8be99f4cf7fb462c4a22f8f335a6
g-onelli <63609012+g-onelli@users.noreply.github.com>
2022-04-16 09:59:09 -0400

    Update customButton.js

--- TaskElephant/components/customButton.js
+++ TaskElephant/components/customButton.js
@@ -3,8 +3,8 @@ import { View, Button, StyleSheet, TouchableOpacity, Text } from "react-native";
 import styleButton from "../styling/buttonStyle";
 import deleteButton from "../styling/DeleteB";
 
-const CustomButton = ({onPress, title,page}) => (
-    <TouchableOpacity onPress={onPress} style={page == 'delete'? deleteButton.customButtonContainer : styleButton.customButtonContainer}>
+const CustomButton = ({onPress, title,page, energycheck}) => (
+    <TouchableOpacity onPress={onPress} style={page == 'delete'? deleteButton.customButtonContainer : styleButton.customButtonContainer || energycheck =='true'? styleButton.energyButtonContainer:styleButton.customButtonContainer}>
         <Text style={styleButton.customButtonText}>{title}</Text>
     </TouchableOpacity>
 )
